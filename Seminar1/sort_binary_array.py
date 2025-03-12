36fdba1e7db612fb3f7489e5b44d5d7eb064d126
ZhidkovMD <103329426+ZhidkovMD@users.noreply.github.com>
2025-03-12 19:10:38 +0300

    Update sort_binary_array.py

--- Seminar1/sort_binary_array.py
+++ Seminar1/sort_binary_array.py
@@ -5,10 +5,10 @@ def sort_binary_array(nums: List[int]) -> None:
     left, right = 0, len(nums) - 1
 
     while left < right:
-        while nums[left] == 0:
+        while left < right and nums[left] == 0:
             left += 1
 
-        while nums[right] == 1:
+        while left < right and nums[right] == 1:
             right -= 1
 
         nums[left], nums[right] = nums[right], nums[left]
